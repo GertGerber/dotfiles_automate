13570bf486506987b7ca879674b64224743e4c1e
GertGerber <ggerber@outlook.co.nz>
2025-08-30 23:58:10 +1200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   group_vars/all.yml
    	modified:   roles/bash/defaults/main.yml
    	deleted:    roles/bash/files/.bashrc
    	deleted:    roles/bash/files/.profile
    	deleted:    roles/bash/files/bash/asciiquarium_aliases.sh
    	deleted:    roles/bash/files/bash/bat_functions.sh
    	deleted:    roles/bash/files/bash/docker_aliases.sh
    	deleted:    roles/bash/files/bash/dotfiles_completions.sh
    	deleted:    roles/bash/files/bash/git_aliases.sh
    	deleted:    roles/bash/files/bash/git_completions.sh
    	deleted:    roles/bash/files/bash/git_functions.sh
    	deleted:    roles/bash/files/bash/gpt_functions.sh
    	deleted:    roles/bash/files/bash/jj_completions.sh
    	deleted:    roles/bash/files/bash/k8s_aliases.sh
    	deleted:    roles/bash/files/bash/k8s_functions.sh
    	deleted:    roles/bash/files/bash/kwctl_completions.sh
    	deleted:    roles/bash/files/bash/lsd_aliases.sh
    	deleted:    roles/bash/files/bash/misc_aliases.sh
    	deleted:    roles/bash/files/bash/neovim_aliases.sh
    	deleted:    roles/bash/files/bash/neovim_functions.sh
    	deleted:    roles/bash/files/bash/paths_functions.sh
    	deleted:    roles/bash/files/bash/paths_vars.sh
    	deleted:    roles/bash/files/bash/pkg_functions.sh
    	deleted:    roles/bash/files/bash/speedtest_functions.sh
    	deleted:    roles/bash/files/bash/taskfile_completions.sh
    	deleted:    roles/bash/files/bash/tasks.sh
    	deleted:    roles/bash/files/bash/terraform_functions.sh
    	deleted:    roles/bash/files/bash/vars.secret_functions.sh
    	deleted:    roles/bash/files/bash/vars.sh
    	deleted:    roles/bash/files/os/Ubuntu/os_functions.sh
    	deleted:    roles/bash/files/themes/axin/axin.theme.sh
    	modified:   roles/bash/handlers/main.yml
    	new file:   roles/bash/meta/main.yml
    	modified:   roles/bash/tasks/main.yml
    	deleted:    roles/bash/tasks/vault.yml
    	deleted:    roles/bash/templates/.bash_private.j2
    	deleted:    roles/bash/uninstall.sh
    	new file:   roles/system/defaults/main.yml
    	new file:   roles/system/handlers/main.yml
    	new file:   roles/system/meta/main.yml
    	new file:   roles/system/tasks/Ubuntu.yml
    	new file:   roles/system/tasks/main.yml
    	new file:   roles/system/templates/user-sudo.j2

--- roles/bash/files/bash/k8s_aliases.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-alias k=kubectl
-alias kctx='kubectl config use-context $(kubectl config get-contexts -o name | fzf)'
-source <(kubectl completion bash)
-complete -F __start_kubectl k
--- roles/bash/files/os/Ubuntu/os_functions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v nala >/dev/null 2>&1; then
-  alias update='sudo nala upgrade -y && sudo nala autoremove -y'
-else
-  alias update='sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y'
-fi
